cc73d56df13d3fffb4767bd5d658b64caff2454c
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-03-08 19:37:51 +0100

    Update RemoveCropsFromFieldController.php
    Completed remove logic

--- app/Http/Controllers/RemoveCropsFromFieldController.php
+++ app/Http/Controllers/RemoveCropsFromFieldController.php
@@ -18,6 +18,10 @@ class RemoveCropsFromFieldController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $field = Field::find($request->segment(2));
+        $crop_id = $request->segment(3);
+        $field->crops()->detach([$crop_id]);
+
 
         // Ta id:na från URL:en och ta bort croppen från rätt field (från databasen). 
         // Både crop-id och fields-id hänger mer i URL:en. 
