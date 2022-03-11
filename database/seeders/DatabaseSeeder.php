3224a348f176320deb43126dfa5c67c22dd5e575
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-03-11 11:36:56 +0100

    More crops in seed and new user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,18 +17,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $crops =[
+        $crops = [
             new Crop(['name' => 'Strawberry', 'incompatible_id' => 2]),
             new Crop(['name' => 'Bluberry', 'incompatible_id' => 3]),
             new Crop(['name' => 'Mangold']),
             new Crop(['name' => 'Salad']),
             new Crop(['name' => 'Beetroot']),
+            new Crop(['name' => 'Bean', 'incompatible_id' => 2]),
+            new Crop(['name' => 'Tomato', 'incompatible_id' => 3]),
+            new Crop(['name' => 'Carrot']),
+            new Crop(['name' => 'Basil']),
+            new Crop(['name' => 'Corn', 'incompatible_id' => 9]),
+            new Crop(['name' => 'Leek', 'incompatible_id' => 10]),
+
         ];
         foreach ($crops as $crop) {
             $crop->save();
         }
 
-        $user = new User(['name' => 'Henrik', 'email' => 'henrik@yrgo.se', 'password' => Hash::make('123')]);
+        $user = new User(['name' => 'Hanna', 'email' => 'hanna@yrgo.se', 'password' => Hash::make('123')]);
         $user->save();
     }
 }
